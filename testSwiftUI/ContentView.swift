b20b4592a9de167a76ad1ad15e266818391c2ecd
Papada Preedagorn <papada.preedagorn@scb.co.th>
2019-11-11 16:54:14 +0700

    Initial Commit

---
+++ testSwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  testSwiftUI
+//
+//  Created by Papada Preedagorn on 11/11/2562 BE.
+//  Copyright © 2562 Papada Preedagorn. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ testSwiftUITests/testSwiftUITests.swift
@@ -0,0 +1,34 @@
+//
+//  testSwiftUITests.swift
+//  testSwiftUITests
+//
+//  Created by Papada Preedagorn on 11/11/2562 BE.
+//  Copyright © 2562 Papada Preedagorn. All rights reserved.
+//
+
+import XCTest
+@testable import testSwiftUI
+
+class testSwiftUITests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
